1384e3d1c529b5432d4a76fd122a1f1562e31b3a
Matt Burke <spraints@gmail.com>
2025-12-31 21:33:57 -0500

    Configure solid queue and mission control

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem "jbuilder"
 
 gem "minitest", "~> 5"
 
+gem "mission_control-jobs"
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "configurations#index"
+  root "configurations#index"
   resources :configurations do
     member do
       put "queued" => "configurations#queue"
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
 
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -22,7 +24,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
