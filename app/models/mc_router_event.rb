bb57d4bdef9eb969a15952e27b953ebfcbb39f9a
Matt Burke <spraints@gmail.com>
2026-01-03 18:43:21 -0500

    extract a method

--- app/models/mc_router_event.rb
+++ app/models/mc_router_event.rb
@@ -2,4 +2,29 @@ class McRouterEvent < ApplicationRecord
   belongs_to :configuration_active_world,
     -> { includes :configuration, :world },
     optional: true
+
+  def self.create_from_json!(raw, remote_ip)
+    data = JSON.parse(raw)
+
+    ev = McRouterEvent.new
+    ev.event = data.fetch("event")
+    ev.occurred_at = DateTime.parse(data.fetch("timestamp"))
+    ev.status = data.fetch("status")
+    ev.client_host = data.fetch("client").fetch("host")
+    ev.client_port = data.fetch("client").fetch("port")
+    ev.hostname = data.fetch("server")
+    ev.player_name = data.dig("player", "name")
+    ev.player_uuid = data.dig("player", "uuid")
+    ev.backend_addr = data.fetch("backend")
+
+    ev.sender_addr = remote_ip
+    ev.raw_payload = raw
+
+    if cur = Configuration.current
+      ev.configuration_active_world = cur.active_worlds.where(hostname: ev.hostname).first
+    end
+
+    ev.save!
+    ev
+  end
 end
