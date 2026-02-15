cc010d9855da3e5650499c4ef78834a11120ae2c
Matt Burke <spraints@gmail.com>
2026-02-15 18:37:50 -0500

    Get more of the concurrency right

--- app/jobs/activate_configuration_job.rb
+++ app/jobs/activate_configuration_job.rb
@@ -1,6 +1,8 @@
 class ActivateConfigurationJob < ApplicationJob
   queue_as :default
 
+  discard_on ConfigurationActivator::Error
+
   def perform(config)
     ConfigurationActivator.apply(config)
   end
