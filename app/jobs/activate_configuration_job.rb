7260906395ac81dfe1b0085d5922947b7071717b
Matt Burke <spraints@gmail.com>
2026-02-15 17:45:14 -0500

    Don't rely on limits_concurrency
    I'm switching to backburner for jobs, because I don't need to run a
    query every 10s for a job that's scheduled like once a month.
    
    This includes the introduction of a "retired" state for
    previously-applied configurations.

--- app/jobs/activate_configuration_job.rb
+++ app/jobs/activate_configuration_job.rb
@@ -1,8 +1,6 @@
 class ActivateConfigurationJob < ApplicationJob
   queue_as :default
 
-  limits_concurrency to: 1, duration: 10.minutes, key: ->(_) { "__all__" }
-
   def perform(config)
     ConfigurationActivator.apply(config)
   end
--- app/models/configuration_activator.rb
+++ app/models/configuration_activator.rb
@@ -7,19 +7,29 @@ class ConfigurationActivator
   end
 
   def self.apply(configuration)
-    mappings = {}
-    configuration.active_worlds.each do |aw|
-      mappings[aw.hostname] = aw.world.backend_addr
+    Configuration.transaction do
+      rows = Configuration.where(id: configuration.parent_id, state: "active").update_all \
+        updated_at: Time.now,
+        state: "retired"
+      if rows != 1
+        Rails.logger.error "error applying configuration #{configuration.id}: its parent is no longer active"
+        raise ActiveRecord::Rollback
+      end
+
+      mappings = {}
+      configuration.active_worlds.each do |aw|
+        mappings[aw.hostname] = aw.world.backend_addr
+      end
+
+      dest_path = Rails.application.config.mc_router_config_path
+      raise "Missing MC_ROUTES environment variable" if dest_path.blank?
+
+      mcrouter_config = JSON.dump({ mappings: mappings })
+      raise "illegal application" unless ok_to_apply?(configuration)
+
+      File.write dest_path, mcrouter_config
+      configuration.update!(state: "applied")
     end
-    dest_path = Rails.application.config.mc_router_config_path
-    raise "Missing MC_ROUTES environment variable" if dest_path.blank?
-    mcrouter_config = JSON.dump({ mappings: mappings })
-    raise "illegal application" unless ok_to_apply?(configuration)
-    File.write dest_path, mcrouter_config
-    configuration.update!(state: "applied")
-  rescue
-    configuration.update!(state: "proposed")
-    raise
   end
 
   private
